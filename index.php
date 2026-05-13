9715ff99ed282bc0cdfe6d9e5ed50d1a899833a2
Williams Alfred Onen <kiwixcompome@gmail.com>
2026-05-13 12:51:26 +0100

    Auto-update:

--- index.php
+++ index.php
@@ -229,7 +229,7 @@ initSession();
                             <path stroke-linecap="round" stroke-linejoin="round" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"/>
                         </svg>
                     </div>
-                    <h3 class="portal-title">New Registration</h3>
+                    <h3 class="portal-title">New Student Registration</h3>
                     <p class="portal-description">Register as a new student and submit your details for ID card generation</p>
                     <a href="register.php" class="portal-link">
                         Get Started
